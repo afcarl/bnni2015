970f61e28a93855e0eee848f39226a81af96c67c
Tambet Matiisen <tambet.matiisen@gmail.com>
2015-07-16 17:57:22 +0300

    Reduced number of cores to 3 to give others also chance.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup srun -c 10 -t 8-0 -p main /storage/software/MATLAB_R2013b/bin/matlab -nodisplay -nosplash -nojvm -r "regression_bayesopt('data/TheMatrix$1.mat', 'traces/$1.mat')" >logs/$1.log &
+nohup srun -c 3 -t 8-0 -p main /storage/software/MATLAB_R2013b/bin/matlab -nodisplay -nosplash -nojvm -r "regression_bayesopt('data/TheMatrix$1.mat', 'traces/$1.mat')" >>logs/$1.log &
